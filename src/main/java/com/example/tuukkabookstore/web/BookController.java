0ca46b08a58d4aea95a610221718e6e9531cad49
Tuukka Grönholm <tmgronholm@gmail.com>
2023-09-13 19:17:29 +0300

    You can add and delete books form the list

--- src/main/java/com/example/tuukkabookstore/web/BookController.java
+++ src/main/java/com/example/tuukkabookstore/web/BookController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.tuukkabookstore.domain.Book;
 import com.example.tuukkabookstore.domain.BookRepository;
 
 
@@ -15,13 +18,32 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
+	// palauttaa booklist sivun ja listaa kaikki kirjat 
 	@GetMapping("/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	// poistaa kirjan ja palauttaa booklist sivua uudelleen 
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	// palauttaa addbook sivun
+	@GetMapping("/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	// tallettaa kirjan ja palauttaa booklist sivun
+	@PostMapping("/save")
+	public String save(Book book) {
+		repository.save(book);
+		return"redirect:booklist";
+	}
 	
-	
+	// palauttaa sivun index
 	@GetMapping("/index")
 	public String Home() {
 		
