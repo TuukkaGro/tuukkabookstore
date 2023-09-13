dda9dae7d92fd76964edcfd7128fcdfeafa79278
Tuukka Grönholm <tmgronholm@gmail.com>
2023-09-13 20:18:53 +0300

    Edit button and edit page

--- src/main/java/com/example/tuukkabookstore/web/BookController.java
+++ src/main/java/com/example/tuukkabookstore/web/BookController.java
@@ -36,6 +36,12 @@ public class BookController {
 		model.addAttribute("book", new Book());
 		return "addbook";
 	}
+	// palauttaa editbook sivun tietylle kirjalle kirjan id:n mukaan
+	@GetMapping("/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+		}
 	// tallettaa kirjan ja palauttaa booklist sivun
 	@PostMapping("/save")
 	public String save(Book book) {
